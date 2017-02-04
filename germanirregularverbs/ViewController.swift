6c6ad4841d54cb78dbdc763a8c68b5b71c1f165a
Jozef Lang <developer@jozeflang.com>
2017-02-04 22:54:44 +0100

    Simplifying application to contain only one ViewController

---
+++ germanirregularverbs/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RootViewController.swift
+//  germanirregularverbs
+//
+//  Created by Jozef Lang on 04/02/2017.
+//  Copyright © 2017 Jozef Lang. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController, UIPageViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
